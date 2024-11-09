ca8d9aacd7ab4fd1019cfc0eeff16ad9916dbd95
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-11-10 08:23:57 +0900

    Add files via upload

---
+++ R2.0q83.X/build/default/production/_ext/1360937237/w65_main.p1.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1360937237/w65_main.p1:  \
+../src/w65_main.c  \
+../src/../src/w65.h  \
+../src/../src/picconfig.h  \
+../src/../fatfs/ff.h  \
+../src/../fatfs/../fatfs/ffconf.h  \
+../src/../drivers/utils.h 

d236f68f7baf03025da12008f40dc48a7c7b8ae6
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-11-09 21:38:07 +0900

    Add files via upload

---
+++ GAME-C/816/mkgame816.bat
@@ -0,0 +1,7 @@
+rem WDC816CC game816.c -A
+WDC816AS -G -L game-c816.asm -o aaa.obj
+WDCLN -HB -g -t aaa.obj -o aaa.bin
+bin2mot -L1780 -ID100 aaa.bin aaa.s
+mot2bin aaa.s GAME-C.B16
+copy GAME-C.B16 ..\..\DISKS\GAME-C\.
+del aaa.*

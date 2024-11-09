24a1fb8480477b30f2a373ad7a08ce23946b3a62
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-11-10 08:24:45 +0900

    Add files via upload

---
+++ R2.0q84.X/build/default/production/_ext/423027577/w65_bd.p1.d
@@ -0,0 +1,13 @@
+build/default/production/_ext/423027577/w65_bd.p1:  \
+../src/boards/w65_bd.c  \
+../src/boards/../../src/w65.h  \
+../src/boards/../../src/../src/picconfig.h  \
+../src/boards/../../src/../fatfs/ff.h  \
+../src/boards/../../src/../fatfs/../fatfs/ffconf.h  \
+../src/boards/../../drivers/SDCard.h  \
+../src/boards/../../drivers/picregister.h  \
+../src/boards/../../drivers/SPI.h  \
+../src/boards/w65_cmn.c  \
+../src/boards/../../drivers/pic18f57q43_spi.c  \
+../src/boards/../../drivers/SDCard.c  \
+../src/boards/../../drivers/../drivers/utils.h 
---
+++ R2.0q84.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=R2.0q84.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/R2.0q84.X.production.hex

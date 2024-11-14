6cfe0114944e6e1f69afc0118c1f9332532abb9a
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-11-14 13:50:53 +0900

    Add files via upload

--- basic/STDALONE/basic65+minmon/mkbasic.bat
+++ basic/STDALONE/basic65+minmon/mkbasic.bat
@@ -1,12 +1,6 @@
-del *.lst
-del *.obj
-del *.map
-del *.sym
-del *.BIN
 WDC02AS -G -L min_mon.asm
 WDCLN -HB -g -t min_mon.obj
 bin2mot -L2900 -ID700 min_mon.bin aaa.s
 mot2bin aaa.s BASIC65.BIN
-copy BASIC65.BIN ..\..\DISKS\STDALONE\.
 del aaa.*
 del min_mon.bin
